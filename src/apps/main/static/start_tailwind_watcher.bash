388bf1380d61b279039ad6de3c4203b762649993
Igor <41533396+BinaryIgor@users.noreply.github.com>
2024-01-07 11:03:36 +0100

    Desired static assets config

--- src/apps/main/static/start_tailwind_watcher.bash
+++ src/apps/main/static/start_tailwind_watcher.bash
@@ -2,4 +2,4 @@
 
 mkdir -p target/assets
 
-./tailwindcss -i assets/styles.css -o target/assets/live-styles.css --watch
+npx tailwindcss -i assets/styles.css -o target/assets/live-styles.css --watch
