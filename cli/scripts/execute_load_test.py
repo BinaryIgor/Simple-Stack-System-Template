0ac09a4a959c98603e25c33bcc6a06492da6a94d
Igor <41533396+BinaryIgor@users.noreply.github.com>
2024-02-05 19:01:48 +0100

    Prepare load test

--- cli/scripts/execute_load_test.py
+++ cli/scripts/execute_load_test.py
@@ -1,23 +1,62 @@
 import math
 import statistics
+import subprocess
 import time
 from concurrent.futures import ThreadPoolExecutor
 
-import subprocess
+from commons import meta
 
-CURL_ARGS = "https://binaryigor.com"
+log = meta.new_log("execute_load_test")
 
-def task(sleep):
+args = meta.cmd_args(env_arg=False,
+                     args_definition={
+                         "requests": {
+                             "required": True,
+                             "type": int
+                         },
+                         "rate_per_second": {
+                             "required": True,
+                             "type": int
+                         },
+                         "parallelism": {
+                             "type": int
+                         },
+                         "curl_endpoint": {
+                             "required": True
+                         },
+                         "curl_auth_header": {
+                             "required": True
+                         }
+                     })
+
+curl_endpoint = args["curl_endpoint"]
+curl_auth_header = args["curl_auth_header"]
+
+curl_cmd = f"curl -s -o /dev/null -H '{curl_auth_header}' {curl_endpoint}"
+
+log.info("About to execute http load test with curl command:")
+print(curl_cmd)
+
+
+def task(sleep, idx):
     if sleep > 0:
         time.sleep(sleep)
 
     start = time.time()
 
+    should_print = idx % rate_per_second == 0
+
+    if should_print:
+        log.info(f"About to execute {idx} request...")
+
     subprocess.run(f"""
     #!/bin/bash
     set -e
-    curl -s -o /dev/null {CURL_ARGS}
+    {curl_cmd}
     """, shell=True, check=True)
+
+    if should_print:
+        log.info(f"{idx} request executed!")
 
     return time.time() - start
 
@@ -50,26 +89,26 @@ def percentile(data, percentile):
     return (data[lower_idx] + data[upper_idx]) / 2
 
 
-rate_per_second = 10
+rate_per_second = args["rate_per_second"]
 delay = 1.0 / rate_per_second
 
-tasks = 100
-parallelism = tasks
+requests = args["requests"]
+parallelism = args.get("parallelism", requests)
 
 start = time.time()
 
 with ThreadPoolExecutor(max_workers=parallelism) as executor:
     futures = []
-    for i in range(tasks):
+    for i in range(requests):
         task_delay = i * delay
-        future = executor.submit(task, task_delay)
+        future = executor.submit(task, task_delay, i)
         futures.append(future)
 
     results = [f.result() for f in futures]
 
 duration = round(time.time() - start, 2)
 
-print(f"{tasks} tasks with {rate_per_second} rate took {duration}")
+print(f"{requests} requests with {rate_per_second} rate took {duration} seconds")
 
 sorted_results = sorted(results)
 
